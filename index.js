1ffa7e2d7b5f314de07a385ab2d73d7207a56f03
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-05-29 22:44:23 +0300

    Show dir

--- index.js
+++ index.js
@@ -4,7 +4,36 @@ const { exec } = require("child_process");
 try {
     const args = core.getInput('args');
 
-    exec(`..\\v0.2.6\\binaries\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
+    // exec(`dir ..\\v0.2.6\\binaries\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
+    exec(`dir ..\\`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        core.info(`stdout: ${stdout}`);
+    });
+
+    exec(`dir ..\\v0.2.7`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        core.info(`stdout: ${stdout}`);
+    });
+
+    exec(`dir ..\\v0.2.7\\binaries`, (error, stdout, stderr) => {
         if (error) {
             core.setFailed(`error: ${error.message}`);
             return;
