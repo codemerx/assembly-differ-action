f0f0c63f9b796899a38bce265f8c37d5ce663f60
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-05-29 23:22:26 +0300

    Removed dir folders

--- index.js
+++ index.js
@@ -4,34 +4,6 @@ const { exec } = require("child_process");
 try {
     const args = core.getInput('args');
 
-    exec(`dir ..\\..\\_actions\\codemerx\\assembly-differ-action\\v0.2.10`, (error, stdout, stderr) => {
-        if (error) {
-            core.setFailed(`error: ${error.message}`);
-            return;
-        }
-
-        if (stderr) {
-            core.setFailed(`stderr: ${stderr}`);
-            return;
-        }
-
-        core.info(`stdout: ${stdout}`);
-    });
-
-    exec(`dir ..\\..\\_actions\\codemerx\\assembly-differ-action\\v0.2.10\\binaries`, (error, stdout, stderr) => {
-        if (error) {
-            core.setFailed(`error: ${error.message}`);
-            return;
-        }
-
-        if (stderr) {
-            core.setFailed(`stderr: ${stderr}`);
-            return;
-        }
-
-        core.info(`stdout: ${stdout}`);
-    });
-
     exec(`..\\..\\_actions\\codemerx\\assembly-differ-action\\v0.2.10\\binaries\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
         if (error) {
             core.setFailed(`error: ${error.message}`);
