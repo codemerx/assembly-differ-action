b64434641854870620eba6c24a4ffec343375ebd
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-05-29 22:56:52 +0300

    Fixed path

--- index.js
+++ index.js
@@ -4,7 +4,63 @@ const { exec } = require("child_process");
 try {
     const args = core.getInput('args');
 
-    exec(`..\\..\\v0.2.8\\_actions\\codemerx\\assembly-differ-action\\binaries\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
+    exec(`dir ..\\..\\`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        core.info(`stdout: ${stdout}`);
+    });
+
+    exec(`dir ..\\..\\_actions`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        core.info(`stdout: ${stdout}`);
+    });
+
+    exec(`dir ..\\..\\_actions\\codemerx`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        core.info(`stdout: ${stdout}`);
+    });
+
+    exec(`dir ..\\..\\_actions\\codemerx\\assembly-differ-action`, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        core.info(`stdout: ${stdout}`);
+    });
+
+    exec(`..\\..\\_actions\\codemerx\\assembly-differ-action\\v0.2.9\\binaries\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
         if (error) {
             core.setFailed(`error: ${error.message}`);
             return;
