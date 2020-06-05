0ca8fa8d367af7e643dae2e442a2ae2b254c3463
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-06-05 21:42:51 +0300

    Changed relative path

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const core = require('@actions/core');
 const { exec } = require("child_process");
 
-try {
-    const args = core.getInput('args');
-    const repository = core.getInput('repository');
-    core.info(`path: ${repository}`);
-
-    exec(`..\\..\\_actions\\codemerx\\assembly-differ-action\\v0.3.1\\binaries\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
-        if (error) {
-            core.setFailed(`error: ${error.message}`);
-            return;
-        }
-
-        if (stderr) {
-            core.setFailed(`stderr: ${stderr}`);
-            return;
-        }
-
-        core.info(`stdout: ${stdout}`);
-    });
-
-    exec(`DIR`, (error, stdout, stderr) =>{
-        core.info(`stdout: ${stdout}`);
-    });
-} catch (error) {
-    core.setFailed(error.message);
+const main = () => {
+    try {
+        const args = core.getInput('args');
+        const assemblyDifferBinaries = core.getInput('assembly-differ-binaries');
+        core.info(`path: ${assemblyDifferBinaries}`);
+    
+        exec(`${assemblyDifferBinaries}\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
+            if (error) {
+                core.setFailed(`error: ${error.message}`);
+                return;
+            }
+    
+            if (stderr) {
+                core.setFailed(`stderr: ${stderr}`);
+                return;
+            }
+    
+            core.info(`stdout: ${stdout}`);
+        });
+    
+        exec(`DIR`, (error, stdout, stderr) =>{
+            core.info(`stdout: ${stdout}`);
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+main();
