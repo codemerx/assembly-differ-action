88b2f8e342cd0d91b8d07bd4577016642e34852a
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-06-15 21:23:44 +0300

    Install assembly-differ tool

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const core = require('@actions/core');
 const { exec } = require("child_process");
 
+const executeCommand = (command, callback) => {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            core.setFailed(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            core.setFailed(`stderr: ${stderr}`);
+            return;
+        }
+
+        try {
+            callback(stdout);
+        } catch (error) {
+            core.setFailed(error.message);
+            throw error;
+        }
+    });
+}
+
+const installAssemblyDiffer = (next) => {
+    executeCommand('dotnet tool install -g assembly-differ', next);
+}
+
+const runAssemblyDiffer = (args) => {
+    executeCommand(`assembly-differ ${args}`, (stdout) => {
+        core.info(`stdout: ${stdout}`);
+    });
+}
+
 const main = () => {
     try {
-        const args = core.getInput('args');
-        const assemblyDifferBinaries = core.getInput('assembly-differ-binaries');
-        core.info(`path: ${assemblyDifferBinaries}`);
-    
-        exec(`${assemblyDifferBinaries}\\assembly-differ.exe ${args}`, (error, stdout, stderr) => {
-            if (error) {
-                core.setFailed(`error: ${error.message}`);
-                return;
-            }
-    
-            if (stderr) {
-                core.setFailed(`stderr: ${stderr}`);
-                return;
-            }
-    
-            core.info(`stdout: ${stdout}`);
+        installAssemblyDiffer(() => {
+            const args = core.getInput('args');
+            runAssemblyDiffer(args);
         });
     } catch (error) {
         core.setFailed(error.message);
